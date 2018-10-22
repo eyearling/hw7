020daf2907cc2a054c80bc40c02b3eda53465360
Emily Yearling <emilyyearling@Emilys-MacBook-Pro.local>
2018-10-22 17:41:35 -0400

    ap.sh edits

--- ap.sh
+++ ap.sh
@@ -5,39 +5,41 @@
 
 # set subject and group identifiers
 set sub j= 02
-set gname = test
+set gname = sub-02
 set session = test
 
 # set data directories
-set top_dir = /scratch/psyc5171/dataset1/sub-${j}/ses-${session}
-set anat_dir  = $top_dir/anat
-set epi_dir   = $top_dir/func
-set stim_dir  = $top_dir/onsets
+set top_dir = /scratch/psyc5171/dataset1/sub-02/ses-test
+set anat_dir = $top_dir/anat
+set epi_dir = $top_dir/func
+
 
 # run afni_proc.py to create a single subject processing script
-afni_proc.py -subj_id $j                                                  \
+afni_proc.py -subj_id sub-02                                                  \
         -script run_afni.sh -scr_overwrite                                    \
         -blocks tshift align tlrc volreg blur mask scale regress             \
-        -copy_anat $anat_dir/sub-${j}_ses-${session}_T1w.nii.gz                          \
-        -dsets $epi_dir/sub-${j}_ses-${session}_task-fingerfootlips_run-0*_bold.nii.gz            \
+        -copy_anat $anat_dir/sub-02_ses-test_T1w.nii.gz                          \
+        -dsets $epi_dir/sub-02_ses-test_task-fingerfootlips_run-0*_bold.nii.gz            \
         -tcat_remove_first_trs 0   -tshift_opts_ts -tpattern alt+z          \
         -tlrc_base MNI_avg152T1+tlrc                                         \
         -volreg_align_to first                                               \
         -volreg_align_e2a                                                    \
         -volreg_tlrc_warp                                                    \
         -blur_size 6.0                                                       \
-        -regress_stim_times /scratch/psyc5171/eay15101/hw7/*.txt  \
+        -regress_stim_times                                                   \
+            /scratch/psyc5171/eay15101/hw7/Finger.txt                           \
+            /scratch/psyc5171/eay15101/hw7/Foot.txt                         \
+            /scratch/psyc5171/eay15101/hw7/Lips.txt                       \
         -regress_stim_labels                                                 \
-            finger foot lips hash filler                                         \
+            Finger Foot Lips Hash Filler                                         \
         -regress_basis 'BLOCK(15)'                                                 \
         -regress_censor_motion 0.5                                           \
         -regress_apply_mot_types demean deriv                                \
         -regress_motion_per_run                                              \
         -regress_opts_3dD                                                    \
-            -gltsym 'SYM: finger -foot' -glt_label 1 finger-foot \
-            -gltsym 'SYM: finger -lips' -glt_label 2 finger-lips \
-            -gltsym 'SYM: foot -lips' -glt_label 3 foot-lips         \
-        words-hash                                                           \
+            -gltsym 'SYM: Finger -Foot' -glt_label 1 Finger-Foot \
+            -gltsym 'SYM: Finger -Lips' -glt_label 2 Finger-Lips \
+            -gltsym 'SYM: Foot -Lips' -glt_label 3 Foot-Lips  \
         -regress_make_ideal_sum sum_ideal.1D                                 \
         -regress_est_blur_epits                                              \
         -regress_est_blur_errts                                              \
